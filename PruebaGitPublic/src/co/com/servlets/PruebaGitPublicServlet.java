a4bfc0994e3bd8420c45fe06369d404f3e8cdc5c
jdelgadop <jdelgadop@taxislibres.com.co>
2015-11-06 09:53:49 -0500

    Initial Import

---
+++ PruebaGitPublic/src/co/com/servlets/PruebaGitPublicServlet.java
@@ -0,0 +1,12 @@
+package co.com.servlets;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class PruebaGitPublicServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
